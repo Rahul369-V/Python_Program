9f7d7cfcfc2c11e75ebcfb3b5a93253884d5ba35
Rahul V <84711895+Rahul369-V@users.noreply.github.com>
2021-11-15 18:55:48 +0530

    Create TWO_STRING_ARE_ANAGRAM_OR_NOT.py

---
+++ TWO_STRING_ARE_ANAGRAM_OR_NOT.py
@@ -0,0 +1,6 @@
+s=input("Enter a string: ") # input read everything as STRING!!!
+y=input("Enter a string: ")
+if(sorted(s)==sorted(y)):
+    print("There are ANAGRAM!!!!!")
+else:
+    print("There are not ANAGRAM!!!!")

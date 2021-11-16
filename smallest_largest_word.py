26a73b943a69b4f64052a91464a9c5782f597787
Rahul V <84711895+Rahul369-V@users.noreply.github.com>
2021-11-16 21:05:10 +0530

    Create smallest_largest_word.py

---
+++ smallest_largest_word.py
@@ -0,0 +1,10 @@
+
+
+
+# PROGRAM TO FIND THE LARGEST AND SMALLEST WORD IN THE STRING
+
+string = "This is an umbrealla" # given string
+print(string)
+l1 = (string.lower()).split()
+print("smallest word in a string:",min(l1))
+print("largest number in a string :",max(l1))

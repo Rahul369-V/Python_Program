95974255fc4385dd311c74f06dca508dc3bc4b87
Rahul V <84711895+Rahul369-V@users.noreply.github.com>
2021-11-22 20:22:02 +0530

    Create Replace.py

---
+++ Replace.py
@@ -0,0 +1,15 @@
+#PROGRAM, REMOVE ALL '*' WITHOUT USING A REPLACE FUNCTION
+
+def fun1(st):
+    #creat a list for storing a element of string
+    newList=[]
+    for j in st:
+        if j != '*':
+            newList.append(j)
+            continue
+    return (''.join(newList))
+
+
+
+s=input("enter a given string:")
+print("string after removing '*' : %s"%fun1(s))
